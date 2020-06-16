2b64f68b0298d34d35fe3d6cdf28b5684fb77fef
Joseph Baca <josephcbaca@gmail.com>
2020-06-15 17:49:54 -0700

    Able to read db file.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 let express = require("express");
 let path = require("path");
+let fs = require("fs");
 
 // Sets up the Express App
 let app = express();
@@ -9,7 +10,7 @@ let PORT = 8080;
 //Set up the Express app to handle data parsing
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
-// This is a request for a static file
+// This is a request for the static folder public
 app.use(express.static(path.join(__dirname, "./public")))
 
 let notes = [];
@@ -25,23 +26,34 @@ app.get("/notes", function (req, res) {
 
 // Displays all characters
 app.get("/api/notes", function (req, res) {
-  return res.json(notes);
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    if (err) throw err;
+    const content = data.toString()
+    let newN
+    // res.end(data);
+  });
+  // return res.json(notes);
 });
 
 app.post("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", function (err, data) {
+    if (err) throw err;
+    const content = data.toString()
+    let newNote = req.body;
+    newNote.title = content.title;
+    newNote.title = content.text;
+    // res.end(data);
+  });
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
   let newNote = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newNote.id = "1"
-  newNote.title = "test"
+  newNote.title = "test" 
+  newNote.text =  "text"
   // newNote.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newNote);
-
-  notes.push(newNote);
 
   res.json(newNote);
 });
